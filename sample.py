a5d9a27852e1db8ccbcf3420a98e247509ac555f
BASKAR121204 <sakuni121204@gmail.com>
2024-10-16 15:57:07 +0530

    second

--- sample.py
+++ sample.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import pytesseract
+import pyttsx3
+from googletrans import Translator
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -7,4 +9,22 @@ image_="Screenshot 2024-07-27 212103.png"
 img=Image.open(image_)
 
 text=pytesseract.image_to_string(img)
+from PIL import Image
+import pytesseract
+import pyttsx3
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+image_="Screenshot 2024-07-27 212103.png"
+img=Image.open(image_)
+
+text=pytesseract.image_to_string(img)
+with open('abc.txt',mode ='w') as file:      
+                 file.write(text) 
 print("The following is the extract text/n",text)
+translator =Translator()
+k=translator.translate(text,dest='german')
+engine = pyttsx3.init() 
+
+engine.say(k)                              
+engine.runAndWait()
